4231d70b7c0df99536731e71f88d286cb4c8fb92
dileepdili <dileepkumardilideepu@gmail.com>
2026-05-20 15:11:35 +0530

    force cookies

--- backend/index.js
+++ backend/index.js
@@ -34,8 +34,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    secure: true, // Force true for Render HTTPS
+    sameSite: 'none' // Force none for cross-domain Vercel requests
   }
 }));
 
